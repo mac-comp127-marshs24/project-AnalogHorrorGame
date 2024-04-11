5f9c2bf6564320a6c7c1345b48f0ca2e0414b042
Moyartu Manley <mmanley@macalester.edu>
2024-04-11 16:01:44 -0500

    Basic structure for inventory

--- src/analoghorror/Inventory.java
+++ src/analoghorror/Inventory.java
@@ -1,13 +1,38 @@
 package analoghorror;
+
+import edu.macalester.graphics.CanvasWindow;
+import edu.macalester.graphics.*;
+
 /**
  * Inventory
  */
-public class Inventory {
-    private static final int SLOT_WIDTH = 30;
-    private static final int SLOT_HEIGHT = 30;
+public class Inventory extends GraphicsGroup{
+    private static final double SLOT_WIDTH = 50;
+    private static final double SLOT_HEIGHT = 50;
 
-    public Inventory(){
+    public Inventory(CanvasWindow canvas){
         /*constructor for inventory obj */
+
+        slotFormation(canvas);
+
     }
+
+    private static void slotInteraction(CanvasWindow canvas){
+
+    }
+
+    private void slotFormation(CanvasWindow canvas){
+        double padding = 0;
+        double x = 0, y = canvas.getHeight()*0.85;
+
+        for(int i = 0; i < 10; i++){
+            Rectangle slot = new Rectangle(x, y, SLOT_WIDTH, SLOT_HEIGHT);
+            canvas.add(slot);
+            x += SLOT_WIDTH + padding;
+        }
+        
+    }
+
+
     
 }
