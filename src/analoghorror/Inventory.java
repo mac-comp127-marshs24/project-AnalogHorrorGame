a7ec716ca5848cbabb97f5b3ae84f43fd87e56e4
Moyartu Manley <mmanley@macalester.edu>
2024-04-11 16:18:38 -0500

    added psuedocode for slotInteraction, slotGenerator functional

--- src/analoghorror/Inventory.java
+++ src/analoghorror/Inventory.java
@@ -12,24 +12,30 @@ public class Inventory extends GraphicsGroup{
 
     public Inventory(CanvasWindow canvas){
         /*constructor for inventory obj */
-
-        slotFormation(canvas);
+        slotGenerator(canvas);
 
     }
 
     private static void slotInteraction(CanvasWindow canvas){
-
+        /*canvas.OnMouseCLick
+         * if slotGroup.getElement at event.getPosition is null do nothing
+         * if there is an element maybe mark true? do some item interaction? or change visuals of inventory slot like border?
+         */
     }
 
-    private void slotFormation(CanvasWindow canvas){
+    /*Basic gist for adding 10 (i think) inventory boxes*/
+    private void slotGenerator(CanvasWindow canvas){
+        GraphicsGroup slotGroup = new GraphicsGroup();
         double padding = 0;
         double x = 0, y = canvas.getHeight()*0.85;
 
         for(int i = 0; i < 10; i++){
             Rectangle slot = new Rectangle(x, y, SLOT_WIDTH, SLOT_HEIGHT);
-            //canvas.add(slot);
+            slotGroup.add(slot);
             x += SLOT_WIDTH + padding;
         }
+
+
         
     }
 
