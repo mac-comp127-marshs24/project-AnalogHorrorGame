ef8c3d5c63dd91a419e0d458b31298d105b7cc14
wkuratana <wkuratan@macalester.edu>
2024-04-11 16:13:12 -0500

    Click functionality draft added

--- src/analoghorror/HorrorGame.java
+++ src/analoghorror/HorrorGame.java
@@ -1,5 +1,6 @@
 package analoghorror;
 import edu.macalester.graphics.*;
+import edu.macalester.graphics.events.MouseButtonEvent;
 
 public class HorrorGame {
     private static final int CANVAS_WIDTH = 854;
@@ -7,15 +8,17 @@ public class HorrorGame {
     
     private static int inventoryHeight = getCanvasHeight() / 7; // Using methods instead while thinking about refactoring l8r
     private static int inventoryWidth = getCanvasWidth() / 8 * 7;
+    private static CanvasWindow canvas;
+    private static Rectangle inventoryBar;
     public static void main(String[] args) {
-        skeletonTest();
+        new HorrorGame();
     }
 
-    public static void skeletonTest() {
-        CanvasWindow canvas = new CanvasWindow("game", CANVAS_WIDTH, CANVAS_HEIGHT);
+    private HorrorGame() {
+        canvas = new CanvasWindow("game", CANVAS_WIDTH, CANVAS_HEIGHT);
 
         // Generally just thinking we work on click/item interactions with some basic shapes for now; move the logic to the proper classes after?
-        Rectangle inventoryBar = new Rectangle(0, 0, inventoryWidth, inventoryHeight);
+        inventoryBar = new Rectangle(0, 0, inventoryWidth, inventoryHeight);
         canvas.add(inventoryBar);
         inventoryBar.setCenter(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 6 * 5);
 
@@ -26,6 +29,7 @@ public class HorrorGame {
         canvas.add(key);
 
         canvas.draw();
+        itemLogic();
 
     }
 
@@ -35,5 +39,16 @@ public class HorrorGame {
 
     public static int getCanvasHeight(){
         return CANVAS_HEIGHT;
+    }
+
+    public void itemLogic(){  // Draft
+        canvas.onClick(event -> {
+            check(event).setCenter((inventoryBar.getCenter()));
+            canvas.draw();
+        });
+    }
+
+    public GraphicsObject check(MouseButtonEvent event){
+        return canvas.getElementAt(event.getPosition());
     }
 }
