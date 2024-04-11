56169e8f2da8d33e2fdee8b6f17b48d0a0742d17
Moyartu Manley <mmanley@macalester.edu>
2024-04-11 15:35:53 -0500

    Added static vars for slot dimensions in inventory

--- src/analoghorror/Inventory.java
+++ src/analoghorror/Inventory.java
@@ -3,6 +3,11 @@ package analoghorror;
  * Inventory
  */
 public class Inventory {
+    private static final int SLOT_WIDTH = 30;
+    private static final int SLOT_HEIGHT = 30;
 
+    public Inventory(){
+        /*constructor for inventory obj */
+    }
     
 }
