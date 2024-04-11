09b031d841b277488afaf75ee00b26728b0d5fb4
dchan9910 <szewingchan883@gmail.com>
2024-04-11 16:32:29 -0500

    added interactable and key obj classes w/ psuedocode

--- src/analoghorror/Room.java
+++ src/analoghorror/Room.java
@@ -3,5 +3,11 @@ package analoghorror;
 /*Like what behavior do we want all rooms to have: probably like a way of naviating out of said room, normalize behaviors of interacting w said room, etc kinda? */
 /*we can maybe do the same w items */
 public interface Room {
+    public void moveBetweenRooms(Room room);
+    //arrow button labelled with destination room
+    //button.onclick
+    //clears canvas of previous room
+    //moves player screen to desination room
+
     
 }
