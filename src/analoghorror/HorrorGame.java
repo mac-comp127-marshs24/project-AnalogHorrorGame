b5c5d1764e08751820c3d3dc7fe48091f86beb3f
Moyartu Manley <mmanley@macalester.edu>
2024-04-11 15:31:31 -0500

    made final static var for canvas resolution

--- src/analoghorror/HorrorGame.java
+++ src/analoghorror/HorrorGame.java
@@ -3,8 +3,10 @@ import edu.macalester.graphics.CanvasWindow;
 //import edu.macalester.graphics.GraphicsObject;
 
 public class HorrorGame {
+    private static final int CANVAS_WIDTH  = 854;
+    private static final int CANVAS_HEIGHT  = 480;
     public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("game", 854, 480);
+        CanvasWindow canvas = new CanvasWindow("game", CANVAS_WIDTH, CANVAS_HEIGHT);
         
         
     }
