5e191a7b504025594e2fd6db55fa06c54759e0eb
wkuratana <wkuratan@macalester.edu>
2024-04-11 15:27:03 -0500

    Changed Window Size

--- src/analoghorror/HorrorGame.java
+++ src/analoghorror/HorrorGame.java
@@ -4,7 +4,7 @@ import edu.macalester.graphics.CanvasWindow;
 
 public class HorrorGame {
     public static void main(String[] args) {
-        CanvasWindow canvas = new CanvasWindow("game", 1280, 720);
+        CanvasWindow canvas = new CanvasWindow("game", 640, 480);
         
         
     }
