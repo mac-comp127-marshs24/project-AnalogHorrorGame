4f72af9f83d30f108b485b8aaf5462963f8c9add
wkuratana <wkuratan@macalester.edu>
2024-04-11 21:11:31 -0500

    Working on click interactions

--- src/analoghorror/HorrorGame.java
+++ src/analoghorror/HorrorGame.java
@@ -12,8 +12,11 @@ public class HorrorGame {
     private Rectangle inventoryBar;
     Rectangle box;
     Ellipse key;
-    boolean boxBool;
-    boolean keyBool;
+    
+    boolean boxBool = false; // ideally these should be in an Item class w/ getters and setters
+    boolean keyBool = false;
+    Point boxHome;
+    Point keyHome;
 
     public static void main(String[] args) {
         new HorrorGame();
@@ -26,6 +29,8 @@ public class HorrorGame {
         inventoryBar = new Rectangle(0, 0, inventoryWidth, inventoryHeight);
         canvas.add(inventoryBar);
         inventoryBar.setCenter(CANVAS_WIDTH / 2, CANVAS_HEIGHT / 6 * 5);
+        boxHome = new Point(300, 100);
+        keyHome = new Point(200, 180);
 
         box = new Rectangle(300, 100, 100, 100);
         canvas.add(box);
@@ -34,7 +39,7 @@ public class HorrorGame {
         canvas.add(key);
 
         canvas.draw();
-        // itemLogic();
+        itemLogic();
 
     }
 
@@ -46,32 +51,45 @@ public class HorrorGame {
         return CANVAS_HEIGHT;
     }
 
-    // public void itemLogic(){  // Draft
-    //     canvas.onClick(event -> {
-    //         if((check(event) == box && boxBool == false) || (check(event) == key && keyBool == false)){
-    //             check(event).setCenter((inventoryBar.getCenter()));
-    //         } else if((check(event) == box && boxBool == true) || (check(event) == key && keyBool == true)) {
-    //             canvas.getElementAt(event.getPosition()).setCenter(canvas.getCenter());
-    //         }
-    //         canvas.draw();
-    //     });
-    // }
+    public void itemLogic(){  // Draft
+        canvas.onClick(event -> {
+            if (check(event) == box && boxBool == false){
+                canvas.getElementAt(event.getPosition()).setCenter((inventoryBar.getCenter())); // proper spot in inventory (use set or smthn)
+                boxBool = true;
+            }
+            if (check(event) == key && keyBool == false){
+                canvas.getElementAt(event.getPosition()).setCenter((inventoryBar.getCenter())); // proper spot in inventory (use set or smthn)
+                keyBool = true;
+            } 
+            if (check(event) == box && boxBool == true) {
+                check(event).setPosition(boxHome); // proper spot in room (track using Item info)
+                boxBool = false;
+            }
+            if (check(event) == key && keyBool == true) {
+                check(event).setPosition(keyHome);; // proper spot in room (track using Item info)
+                keyBool = false;
+            }
+            canvas.draw();
+        });
+    }
 
-    // public GraphicsObject check(MouseButtonEvent event){
-    //     GraphicsObject item = canvas.getElementAt(event.getPosition());
-    //     // maybe also add a bool to each Item object so we can check if it's in the inventory
-    //     // that way we can change behavior
-    //     if(item == box && boxBool == true){
-    //         boxBool = false;
-    //     } else if (item == key && keyBool == true) {
-    //         keyBool = false;
-    //     }
-    //     else{
-    //         if (item == box) {
-    //             boxBool = true;
-    //         } else if(item == key)
-    //             keyBool = true;
-    //     }
-    //     return item;
-    // }
+    public GraphicsObject check(MouseButtonEvent event){
+        GraphicsObject item = canvas.getElementAt(event.getPosition());
+        // maybe also add a bool to each Item object so we can check if it's in the inventory
+        // that way we can change behavior
+        // if (item == box && boxBool == true) {
+        //     System.out.println("boxBool is now " + boxBool);
+        // } 
+        // if (item == key && keyBool == true) {
+        //     System.out.println("keyBool is now " + keyBool);
+        // } else {
+        //     if (item == box) {
+        //         System.out.println("boxBool is now " + boxBool);
+        //     } 
+        //     if (item == key) {
+        //         System.out.println("keyBool is now " + keyBool);
+        //     }
+        // }
+        return item;
+    }
 }
